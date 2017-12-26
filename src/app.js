3752bcb880e43f49bbc22cd024613c0ca804c2aa
Hafiz <hafizdidarali@gmail.com>
2017-12-26 11:53:06 +0000

    working version after removing background and adding app.js

---
+++ src/app.js
@@ -0,0 +1,13 @@
+// import angular from 'angular';
+// import 'angular-material'
+// import 'angular-material/angular-material.css';
+// import angularAnimate from 'angular-animate';
+// import angularMaterial from 'angular-material';
+// import angularAria from 'angular-aria';
+// import uirouter from '@uirouter/angularjs';
+//
+// import routing from './app.config';
+//
+// angular.module('app', [uirouter,angularMaterial, angularAnimate, angularAria])
+//     .config(routing);
+// console.log('working');
--- webpack.config.js
+++ webpack.config.js
@@ -20,9 +20,9 @@ if (fileSystem.existsSync(secretsPath)) {
 
 var options = {
   entry: {
+    app: path.join(__dirname, "src", "app.js"),
     popup: path.join(__dirname, "src", "js", "popup.js"),
-    options: path.join(__dirname, "src", "js", "options.js"),
-    background: path.join(__dirname, "src", "js", "background.js")
+    options: path.join(__dirname, "src", "js", "options.js")
   },
   output: {
     path: path.join(__dirname, "build"),
@@ -77,11 +77,6 @@ var options = {
       template: path.join(__dirname, "src", "options.html"),
       filename: "options.html",
       chunks: ["options"]
-    }),
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, "src", "background.html"),
-      filename: "background.html",
-      chunks: ["background"]
     }),
     new WriteFilePlugin()
   ]
