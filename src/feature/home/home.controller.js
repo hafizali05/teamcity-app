8b38b96965f4de1e696324513124ea4872bc38b7
hafiz didarali <hafizdidarali@gmail.com>
2018-01-01 22:07:54 +0000

    notification working

--- src/feature/home/home.controller.js
+++ src/feature/home/home.controller.js
@@ -3,12 +3,40 @@ export default class HomeController {
         this.auth = authentication;
     }
     randomName(data) {
+        console.log(chrome)
+        var ValidURL = {
+            type : 'basic',                // type of notification we can change it in chrome developer tools
+            iconUrl: 'icon-48.png',          // icon appear for limit
+            title: 'Status',
+            message: "dope! its working!"
+        };
+
+        var InValidURL = {
+            type : 'basic',                // type of notification we can change it in chrome developer tools
+            iconUrl: 'icon-48.png',          // icon appear for limit
+            title: 'Status',
+            message: "Fuck! get your fucking URL straight!"
+        };
+        // chrome.storage.sync.
+
+        function callback() {
+            console.log('working notification');
+        }
+        //
         this.auth.validateURL(data.teamcityURL)
-            .then(function (data) {
-                console.log(data);
+            .then(function (response) {
+                if(response.status === 200){
+                    chrome.notifications.create(ValidURL);
+                } else {
+                    chrome.notifications.create(InValidURL);
+                }
             }).catch(function (eror) {
                 console.log(eror);
             })
+
+
+
+
         // console.log('res from randomName',res);
         // if(res){
         //     console.log('authenticated');
