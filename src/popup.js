cf74d0bd63c6f5391de136f15220cd0e0c7db587
hafiz didarali <hafizdidarali@gmail.com>
2018-01-08 22:54:45 +0000

    cookie validation
    1. pop up when open checks if there is a cookie exists
    2. if cookie exists than move to logged in page else be on the first page.

--- src/popup.js
+++ src/popup.js
@@ -5,18 +5,41 @@ import 'angular-material/angular-material.css';
 import angularAnimate from 'angular-animate';
 import angularMaterial from 'angular-material';
 import angularAria from 'angular-aria';
+import ngCookie from 'angular-cookies';
 import uirouter from '@uirouter/angularjs';
 import routing from './app.config';
 import themeSettings from './themeSettings';
 import loginPage from './feature/home';
 import Settings from './feature/settings';
-angular.module('teamcityApp', [loginPage, Settings,  uirouter, angularMaterial, angularAnimate, angularAria])
+angular.module('teamcityApp', [loginPage, Settings,  uirouter, angularMaterial, angularAnimate, angularAria, ngCookie])
     .controller('teamcityApp',function ($scope) {
-        $scope.greeting = "welcome";
-        console.log('chrome  cookies',chrome);
-        chrome.cookies.getAll({domain:'teamcity.keytree.net'},function(cookies){
-                console.log('cookies:',cookies);
-        })
     })
     .config(routing)
-    .config(themeSettings);
+    .config(themeSettings)
+    .run(['$rootScope', '$transitions', '$state', '$cookies', '$http',
+        function ($rootScope, $transitions, $state, $cookies, $http) {
+            chrome.cookies.getAll({},(cookies)=>{
+                console.log(cookies);
+                let cookie = cookies.find(ele => ele.name === "TCSESSIONID");
+                console.log(cookie);
+                if(cookie < 1 || cookie === undefined){
+                    $state.go('home');
+                } else {
+                    $state.go('settings');                    
+                }
+            });
+            // $transitions.onStart({
+            //     to: function (state) {
+            //         console.log(chrome.cookies);
+            //         chrome.cookies.getAll({},(cookies)=>{
+            //             console.log(cookies);
+            //             let cookie = cookies.find(ele => ele.name === "TCSESSIONID");
+            //             console.log(cookie);
+            //             if(cookie < 1 || cookie === undefined){
+            //                 $state.go('home');
+            //             }
+            //         });
+            //     }
+            // });
+        }]
+    );
