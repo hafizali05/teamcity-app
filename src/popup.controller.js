1ebffb126af3c5bcca5c6d7bc0e3780692449811
hafiz didarali <hafizdidarali@gmail.com>
2018-02-04 06:38:09 +0000

    making logout button work

--- src/popup.controller.js
+++ src/popup.controller.js
@@ -1,10 +1,20 @@
 export default class PopupController {
     constructor($scope,authentication,$rootScope){
+        this.$scope = $scope;
+        this.$rootScope = $rootScope;
         console.log('$rootScope popup controller',$rootScope);
         $scope.$watch('$root.loggedIn',function(){
-            console.log('when logged in event',$rootScope.loggedIn);
             $scope.loggedIn = $rootScope.loggedIn;
-        })
+        });
+        // $scope.logout = function(){
+        //     $rootScope.loggedIn = false;                    
+        //     $state.go('home');            
+        // }        
+    }
+    logout() {
+        console.log('working');
+        // this.$rootScope.loggedIn = false;                    
+        // this.$state.go('home');                    
     }
 }
-PopupController.$inject = ['$scope','authentication','$rootScope'];
+PopupController.$inject = ['$scope','authentication','$rootScope','$state'];
