5d7313e9bf60bb6e9048745b90534ded900d0c42
Hafiz <hafizdidarali@gmail.com>
2018-03-17 09:08:05 +0000

    removed commented code.

--- src/feature/buildLists/buildlists.controller.js
+++ src/feature/buildLists/buildlists.controller.js
@@ -3,46 +3,7 @@ export default class BuildListsController {
         this.$scope = $scope;
         this.$rootScope = $rootScope;
         this.auth = authentication;
-        this.$state = $state;        
-        // (function () {
-        //     'use strict';
-        //      var alarmName = 'remindme';
-        //      function checkAlarm(callback) {
-        //        chrome.alarms.getAll(function(alarms) {
-        //          var hasAlarm = alarms.some(function(a) {
-        //            return a.name == alarmName;
-        //          });
-        //          var newLabel;
-        //          if (hasAlarm) {
-        //            newLabel = 'Cancel alarm';
-        //          } else {
-        //            newLabel = 'Activate alarm';
-        //          }
-        //          document.getElementById('toggleAlarm').innerText = newLabel;
-        //          if (callback) callback(hasAlarm);
-        //        })
-        //      }
-        //      function createAlarm() {
-        //        chrome.alarms.create(alarmName, {
-        //          delayInMinutes: 0.1, periodInMinutes: 0.1});
-        //      }
-        //      function cancelAlarm() {
-        //        chrome.alarms.clear(alarmName);
-        //      }
-        //      function doToggleAlarm() {
-        //        checkAlarm( function(hasAlarm) {
-        //          if (hasAlarm) {
-        //            cancelAlarm();
-        //          } else {
-        //            createAlarm();
-        //          }
-        //          checkAlarm();
-        //        });
-        //      }
-        //      var toggleAlarm = document.getElementById('toggleAlarm');
-        //      toggleAlarm.addEventListener('click', doToggleAlarm);
-        //     checkAlarm();
-        //   })();        
+        this.$state = $state;         
     }
     startAlarm(){
         debugger;
