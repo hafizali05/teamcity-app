c824afe8c130471d27e39ccd2b0329ab5cc49c2c
Hafiz <hafizdidarali@gmail.com>
2018-03-24 20:31:40 +0000

    onchanged working and updating buil data working

--- src/feature/buildLists/buildlists.controller.js
+++ src/feature/buildLists/buildlists.controller.js
@@ -9,21 +9,47 @@ export default class BuildListsController {
         this.$state = $state;
         this.$rootScope = $rootScope;
         this.$http = $http;
-        this.$scope.buildlist = null;
-        this.teamcity = null;        
+        this.$scope.allbuilds = null;
     }
     $onInit() {
+        this.onChanged();
         const chromeStorage = chrome.storage.sync;
         chromeStorage.get('teamcity',(ele)=>{
             this.populateBuilds(ele);
         })        
     }
     populateBuilds(ele){
-        console.log(ele);
         var url = `${ ele.teamcity.teamcityURL }/httpAuth/app/rest/builds`;
         this.$http.get(url)
             .then((response)=>{
-                this.$scope.buildlist = response.data.build;
+                this.$scope.allbuilds = response.data.build;
             })
     }
+    startWatch(data){
+        chrome.storage.sync.get('teamcity',(ele)=>{
+            var userdata = ele;
+            console.log('userdata:',userdata.teamcity.build);
+            if(Array.isArray(userdata.teamcity.build)){
+                userdata.teamcity.build.push(data);
+                console.log('userdata has build:',userdata);
+                chrome.storage.sync.set({
+                    'teamcity':userdata.teamcity
+                },(error)=>{
+                    console.log(error);
+                })                
+            } else {
+                console.log('userdata does not have build:',userdata);                
+                userdata.teamcity.build = [data];
+                chrome.storage.sync.set(userdata,(error)=>{
+                    console.log(error);
+                })                
+            }
+        })
+    }
+    
+    onChanged(){
+        chrome.storage.onChanged.addListener(function(changes, nameSpace){
+            console.log('changes, nameSpace',changes, nameSpace);
+        });        
+    }    
 }
